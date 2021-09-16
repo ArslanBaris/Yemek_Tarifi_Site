4d384fe7e4b562daaef1bb80c5b123944eaf38ba
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-09-16 22:32:13 +0300

    feat : Sql Connection class added.

---
+++ Yemek_Tarifleri_Site/App_Code/SqlSinif.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+/// <summary>
+/// Summary description for SqlSinif
+/// </summary>
+public class SqlSinif
+{
+		public SqlConnection baglanti(){
+            SqlConnection baglan = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=Dbo_yemektarifi;Integrated Security=True");
+            baglan.Open();
+            return baglan;
+        }
+    
+	
+}
